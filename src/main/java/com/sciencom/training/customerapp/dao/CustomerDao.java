18142ab3f8b317877c5d3e60feee4c2495461395
Windu Purnomo <wpwindupurnomo@gmail.com>
2015-11-20 10:11:56 +0700

    initial commit

---
+++ src/main/java/com/sciencom/training/customerapp/dao/CustomerDao.java
@@ -0,0 +1,14 @@
+package com.sciencom.training.customerapp.dao;
+
+import java.util.List;
+
+import com.sciencom.training.customerapp.model.Customer;
+
+public interface CustomerDao {
+	public void create(Customer customer);
+	public List<Customer> read();
+	public void update(Customer customer);
+	public void delete(Customer customer);
+	
+	public Customer getByEmail(String email);
+}
---
+++ src/main/java/com/sciencom/training/customerapp/model/Customer.java
@@ -0,0 +1,57 @@
+package com.sciencom.training.customerapp.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="customer")
+public class Customer {
+	
+	@Id
+	private String id;
+	private String name;
+	private String email;
+	private String phone;
+	
+	@OneToMany(mappedBy="customer", fetch = FetchType.LAZY, cascade={CascadeType.ALL})
+	private List<Pemesanan> orders;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public List<Pemesanan> getOrders() {
+		return orders;
+	}
+	public void setOrders(List<Pemesanan> orders) {
+		this.orders = orders;
+	}
+	
+	
+}
